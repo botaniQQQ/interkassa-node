1cf67b283a5cf2668f145c3174977ea162f3f8a6
botaniQQQ <botaniqqq@hotmail.com>
2019-06-28 15:20:23 +0300

    Only ik_ params

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ module.exports = (params, key, signature = false, free = false) => {
     if (!params || !key || typeof params !== 'object') {
         throw new Error('Missing params and secret key');
     }
+    params = JSON.parse(JSON.stringify(params));
     params.ik_am = parseFloat(params.ik_am).toString();
+    Object.keys(params).forEach(function (p) {
+        if (!/^ik_/i.test(p)) {
+            delete params[p];
+        }
+    });
     let result = {};
     if (signature) {
         let params_arr = [];
         delete params.ik_sign;
-        Object.keys(params).sort().forEach(function (key) {
-            params_arr.push(params[key]);
+        Object.keys(params).sort().forEach(function (p) {
+            params_arr.push(params[p]);
         });
         params_arr.push(key);
         let params_str = params_arr.join(':');
@@ -29,8 +35,8 @@ module.exports = (params, key, signature = false, free = false) => {
     }
 
     let url = free ? 'https://www.free-kassa.ru/merchant/cash.php?' : 'https://sci.interkassa.com/?';
-    Object.keys(params).forEach(function (key) {
-        url += key + '=' + encodeURIComponent(params[key]) + '&';
+    Object.keys(params).forEach(function (p) {
+        url += p + '=' + encodeURIComponent(params[p]) + '&';
     });
 
     result.url = url;
